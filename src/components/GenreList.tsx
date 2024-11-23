7457cef1ac348e0a4f5c6efe2124f1b6838b7df6
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2024-11-23 15:06:30 +0300

    spinner added for waiting on the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,20 +1,15 @@
 import useGenres from "../hooks/useGenres";
-import {
-  HStack,
-  Image,
-  List,
-  ListItem,
-  SkeletonText,
-  Text,
-} from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   //   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   return (
     <>
-      {error && <Text>{error}</Text>}
       <List>
         {/* {isLoading &&
           skeletons.map((skeleton) => <SkeletonText key={skeleton} />)} */}
